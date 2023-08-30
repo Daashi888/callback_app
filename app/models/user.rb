96ab10c500c53d28e2611c48f59116036e98ffda
Kenji <fcb141021@yahoo.co.jp>
2023-08-31 00:53:13 +0900

    複数指定のコールバック

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,10 @@
 class User < ApplicationRecord
-  before_create :ensure_has_name
-
   
-  after_initialize do |user|
-    p user
-    puts "オブジェクトを生成しました！"
-    user.name = "Taro"
+  before_save do
+    puts '１番目に呼ばれたコールバック'
   end
 
-  private
-
-  def ensure_has_name
-    puts "nameの値をTaroに設定します！"
-    self.name = 'Taro' if name.blank?
+  before_save do
+    puts '２番目に呼ばれたコールバック'
   end
 end
